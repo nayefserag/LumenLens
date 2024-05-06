981f062cc718f20dbca6e9d126cc66eede142c04
nayefserag1 <nayf.serag@mahaseel.net>
2024-05-06 15:18:36 +0300

    Update Post Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,37 +4,54 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-
+use Laravel\Lumen\Routing\Controller;
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
+        if ($posts->count() < 1) {
+            return response()->json('No posts found!');
+        }
         return response()->json($posts);
     }
 
     public function store(Request $request)
     {
         $post = Post::create($request->all());
-        return response()->json($post, 201);
+        return response()->json(['message' => 'Post created successfully!', 'data' => $post], 201);
     }
 
-    public function show($id)
+    public function show($post_id)
     {
-        $post = Post::findOrFail($id);
+        $post = Post::findOrFail($post_id);
         return response()->json($post);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $post_id)
     {
-        $post = Post::findOrFail($id);
+        $post = Post::find($post_id);
         $post->update($request->all());
-        return response()->json($post);
+        return response()->json(['message' => 'Post updated successfully!', 'data' => $post], 201);
     }
 
-    public function destroy($id)
+    public function destroy($post_id)
     {
-        Post::destroy($id);
-        return response()->json(null, 204);
+        $post = Post::findOrFail($post_id);
+        $post->delete();
+    
+        return response()->json(['message' => 'Post deleted successfully']);
     }
+
+    public function restore($post_id)
+{
+    $post = Post::withTrashed()->findOrFail($post_id);
+    if (!$post->trashed()) {
+        return response()->json(['message' => 'Post is not in trash']);
+    }
+    $post->restore();
+
+    return response()->json(['message' => 'Post restored successfully']);
+}
+
 }
