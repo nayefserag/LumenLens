2ee28a82db5104b6459c400a58530abce9daf818
nayefserag1 <nayf.serag@mahaseel.net>
2024-05-06 15:22:51 +0300

    Update Post Model & add comments_ids to model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'author'];
+    use SoftDeletes;
+    protected $fillable = ['title', 'content', 'author', 'comment_ids'];
+    protected $casts = [
+        'comment_ids' => 'array'
+    ];
+
+    public function comments()
+    {
+        return Comment::findMany($this->comment_ids);
+    }
 }
